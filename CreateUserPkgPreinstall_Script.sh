8c8377e6db6d3bb92cb1d898f13b8ee07ba78719
A.Y. Siu <aysiu@users.noreply.github.com>
2016-04-05 10:12:24 -0700

    Check to see if user is logged in

--- CreateUserPkgPreinstall_Script.sh
+++ CreateUserPkgPreinstall_Script.sh
@@ -23,34 +23,46 @@ if [ "$accountSearch" == "$shortUsername" ]; then
 
          #printf "$shortUsername has $oldUserID instead of $desiredID.\n"
 
-         # Make sure the desired ID is available
-         currentOwner=$(dscl . -list /Users UniqueID | grep "$desiredID")
+         # Make sure the user isn't logged in
+         loggedInUser=$(ls -l /dev/console | awk '/ / { print $3 }')
 
-         if [ -z "$currentOwner" ]; then
-         
-            #printf "$desiredID is available. Changing $shortUsername ID to $desiredID.\n"
-            
-            # Change the user ID
-            sudo dscl . -change /Users/"$shortUsername" UniqueID "$oldUserID" "$desiredID"
-
-            # See if the directory exists (the user may have been created but never logged in
-            if [ -d "/Users/$shortUsername" ]; then
-            
-               #printf "/Users/$shortUsername exists, so changing ownership to $desiredID.\n"
-               
-               # Change ownership to the new ID
-               sudo chown -R "$desiredID" /Users/"$shortUsername"
-            fi
-
-         else
-         
-            # If the user ID isn't available, then abort
-            printf "User ID $desiredID is already taken by another user.\n"
-           exit 1      
-           
-         # End checking to see if the desired ID is available
-         fi   
+         if [ "$loggedInUser" == "$shortUsername" ]; then
    
+            printf "$shortUsername is logged in. Cannot proceed.\n"
+            exit 1
+         
+         else
+            # Make sure the desired ID is available
+            currentOwner=$(dscl . -list /Users UniqueID | grep "$desiredID")
+
+            if [ -z "$currentOwner" ]; then
+         
+               #printf "$desiredID is available. Changing $shortUsername ID to $desiredID.\n"
+            
+               # Change the user ID
+               sudo dscl . -change /Users/"$shortUsername" UniqueID "$oldUserID" "$desiredID"
+
+               # See if the directory exists (the user may have been created but never logged in
+               if [ -d "/Users/$shortUsername" ]; then
+            
+                  #printf "/Users/$shortUsername exists, so changing ownership to $desiredID.\n"
+               
+                  # Change ownership to the new ID
+                  sudo chown -R "$desiredID" /Users/"$shortUsername"
+               fi
+
+            else
+         
+               # If the user ID isn't available, then abort
+               printf "User ID $desiredID is already taken by another user.\n"
+               exit 1      
+           
+            # End checking to see if the desired ID is available
+            fi   
+      
+         # End checking to see if the user is logged in
+         fi
+      
       # End checking to see if user has wrong ID
       fi
 
