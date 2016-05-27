c7c3cee26a198c4d888d4f57cc7d465d696855f6
A.Y. Siu <aysiu@users.noreply.github.com>
2016-05-27 10:25:29 -0700

    Create munki_repo_ip.sh

---
+++ ConditionScripts/munki_repo_ip.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#### Gets your Munki repo's IP address, which may change depending on whether the client is connected to your network internally or not
+
+## Fill in your Munki repo's fqdn
+MUNKI_REPO='subdomain.yourdomain.com'
+
+## Borrowing some variable setup from Tim Sutton: https://github.com/timsutton/munki-conditions
+DEFAULTS=/usr/bin/defaults
+MUNKI_DIR=$($DEFAULTS read /Library/Preferences/ManagedInstalls ManagedInstallDir)
+COND_DOMAIN="$MUNKI_DIR/ConditionalItems"
+
+## Getting the IP address of the Munki repo domain name
+MUNKI_REPO_IP=$(Nslookup "$MUNKI_REPO" | grep "Address: " | awk -F "Address: " '{print $2}')
+
+## Writing back the fetched IP address
+$DEFAULTS write "$COND_DOMAIN" munki_repo_id -string "$MUNKI_REPO_IP"
