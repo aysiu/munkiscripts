c9a60e6161e594c593870b5c3b4c180171342610
A.Y. Siu <aysiu@users.noreply.github.com>
2016-06-29 10:13:30 -0700

    Create UserMustBeLoggedIn.sh

---
+++ UserMustBeLoggedIn.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+## This is a preinstall_script for a pkginfo file to not install the item unless a user is logged in.
+
+# Get the state of the lastuser
+lastUser=$(defaults read /Library/Preferences/com.apple.loginwindow.plist lastUser)
+
+# Check if the last user is logged in...
+if [ ! -z "$lastUser" ] && [ "$lastUser" == "loggedIn" ]; then
+
+  # If the last user is logged in, proceed...
+  exit 0
+   
+else
+  
+  # Otherwise abort installing this item
+   exit 1
+   
+fi
