e3a00c85227cef45927af01f1d8fde1824b29eb8
A.Y. Siu <aysiu@users.noreply.github.com>
2016-06-29 10:21:08 -0700

    Added warning

--- UserMustBeLoggedIn.sh
+++ UserMustBeLoggedIn.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ## This is a preinstall_script for a pkginfo file to not install the item unless a user is logged in.
+## Warning: Absolutely be sure that you don't make this a preinstall_script for an item that requires a logout or reboot in order to install!!!
 
 # Get the state of the lastuser
 lastUser=$(defaults read /Library/Preferences/com.apple.loginwindow.plist lastUser)
