338132c8e3af2234f18cd44cdb832a86e172f271
A.Y. Siu <aysiu@users.noreply.github.com>
2016-08-09 11:08:10 -0700

    Check version and existence of recovery HD

--- RecoveryPartitionInstallCheckScript.sh
+++ RecoveryPartitionInstallCheckScript.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
-# Create a variable to see if a recovery partition exists
+# Fill in the version number to check for
+versionToCheck="10.11.6"
 
-testVariable=$(diskutil list | grep "Apple_Boot")
+# First, check to see if there is a recovery partition
 
-# If it's empty, it's not installed
-if [ -z "$testVariable" ]; then
+partitionCheck=$(diskutil list | grep "Recovery HD")
 
+# See if it's empty
+if [ -z "$partitionCheck" ]; then
+
+	# Since it's empty, the recovery partition is not installed
 	exit 0
 
-# If it's not empty, it's installed
-
+# If it's not empty, it may be installed... let's check the receipt version
 else
-
-	exit 1
+	# Get the version of the recovery partition installed
+	recoveryVersion=$(pkgutil --pkg-info se.gu.it.RecoveryPartitionInstaller.pkg | grep "version" | awk -F ": " '{print $2}')
 	
+	# Check the version
+	if [ "$recoveryVersion" == "$versionToCheck" ]; then
+		# It's the matching version, so installed
+		exit 1
+
+	else
+		# Version doesn't match or there is no .pkg receipt, it's not installed
+		exit 0
+
+	# End checking for .pkg version
+	fi
+
+# End checking for existence of recovery partition
 fi
