cdd246b047978fb44d2038ffd828ffd260d54096
A.Y. Siu <aysiu@users.noreply.github.com>
2016-10-21 14:32:36 -0700

    Update enroll.php

--- munki-enroll/munki-enroll/enroll.php
+++ munki-enroll/munki-enroll/enroll.php
@@ -1,23 +1,3 @@
 <?php
-
-// Specify the template source to be copied as an individual template
-$source="../manifests/individual_template";
-
-// Get the varibles passed by the enroll script
-$identifier = $_GET["identifier"];
-$hostname   = $_GET["hostname"];
-
-// Create destination
-$destination="../manifests/" . $hostname;
-
-// Check if manifest already exists for this machine
-if ( file_exists($destination) )
-    {
-        echo "Computer manifest " . $destination . " already exists.";
-    }
-else
-    {
-        echo "Computer manifest does not exist. Will create " . $destination;
-        copy($source, $destination);
-    }
+// This file has moved to https://github.com/aysiu/munki-serial-enroll/blob/master/munki-enroll/enroll.php
 ?>
