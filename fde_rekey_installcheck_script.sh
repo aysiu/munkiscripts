ee2322e621e38177887bda33b5fbdccda7b32b4e
aysiu <aysiu@users.noreply.github.com>
2017-09-11 10:12:25 -0700

    Create fde_rekey_installcheck_script.sh

---
+++ fde_rekey_installcheck_script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# .plist location
+plist='/var/root/fderekey.plist'
+
+# Double-check that the .plist exists
+if [[ -f "$plist" ]]; then
+
+	# Since it exists, let's get the current recovery key
+	recovery=$(/usr/bin/defaults read "$plist" RecoveryKey)
+
+	# Let's test it to make sure it's valid
+	output=$(/usr/bin/expect -c "
+		spawn /usr/bin/fdesetup validaterecovery
+		expect \"Enter the current recovery key:\"
+		send \"$recovery\n\"
+		expect eof")
+
+	# See if the output contains the word "true"
+	output_test=$(echo "$output" | grep "true")
+
+	if [[ -z "$output_test" ]]; then
+
+		# If "true" wasn't in there, it's not installed
+		#"Echo it's not installed"
+		exit 0
+
+	else
+		# If "true" was in there, it's installed
+		#echo "It's installed"
+		exit 1
+
+	fi
+
+else
+
+	# If the .plist doesn't exist, it's not installed
+	#echo "$plist does not exist, so it's not installed"
+	exit 0
+
+fi
