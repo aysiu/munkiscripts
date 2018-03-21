c7fb62315027e6624ecc7eb87224ab858c3bb4ff
aysiu <aysiu@users.noreply.github.com>
2018-03-21 12:38:03 -0700

    Full path to binary

--- AddOptionalInstalls.sh
+++ AddOptionalInstalls.sh
@@ -19,18 +19,18 @@ optionalDefaults=("Firefox"
 
 # Check to see if the file exists. If it doesn't, you may have to create it with an empty array; otherwise, 
 if [ ! -f "$manifestLocation" ]; then
-   sudo "$plistBuddy" -c "Add :managed_installs array" "$manifestLocation"
+   "$plistBuddy" -c "Add :managed_installs array" "$manifestLocation"
 fi
 
 for packageName in "${optionalDefaults[@]}"
    do
       # Check it's not already in there
-      alreadyExists=$("$plistBuddy" -c "Print: managed_installs" "$manifestLocation" | grep "$packageName")
+      alreadyExists=$("$plistBuddy" -c "Print: managed_installs" "$manifestLocation" | /usr/bin/grep "$packageName")
 
       # Single quote expansion of variables gets messy in bash, so we're going to pre-double-quote the single-quotes on the package name
       alteredPackageName="'""$packageName""'"
    
       if [ -z "$alreadyExists" ]; then
-         sudo "$plistBuddy" -c "Add :managed_installs: string $alteredPackageName" "$manifestLocation"
+         "$plistBuddy" -c "Add :managed_installs: string $alteredPackageName" "$manifestLocation"
       fi
    done
